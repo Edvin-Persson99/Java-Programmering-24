2dba3b0c20cb45a0bbbda049c52f254608dc35ef
Edvin-Persson <eddepersson@gmail.com>
2024-08-27 16:05:15 +0200

    Create E2.java

---
+++ E2.java
@@ -0,0 +1,15 @@
+public class E2 {
+
+    public static void main(String[] args) {
+   
+     
+       for(int i = 1; i<= 19; i++){
+        if (!(i%2==0)) {
+            System.out.print(i +" ");
+        }
+       }
+
+
+    }
+
+}
