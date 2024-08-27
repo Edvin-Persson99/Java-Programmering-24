c9c7aab5fe31d3d237c7d92bf2c1a9f89b944f6d
Edvin-Persson <eddepersson@gmail.com>
2024-08-27 09:11:42 +0200

    Create E1.java

---
+++ exercises/E1.java
@@ -0,0 +1,10 @@
+public class E1 {
+
+    public static void main(String[] args) {
+        Integer myAge = 25;
+        String myFirstName = "Edvin";
+
+        System.out.println("My name is "+myFirstName+" and my age is "+myAge);
+    }
+
+}
