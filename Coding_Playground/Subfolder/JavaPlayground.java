8ed7c9fba294807de548ba38fc1d89af675cd311
Edvin-Persson <eddepersson@gmail.com>
2024-08-30 14:07:45 +0200

    Added a file that gives me the length of my name

---
+++ Coding_Playground/Subfolder/JavaPlayground.java
@@ -0,0 +1,10 @@
+package Coding_Playground.Subfolder;
+
+class JavaPlayground {
+
+    public static void main(String[] args) {
+        String name = "Edvin";
+        int nameLength = name.length();
+        System.out.println("The length of my name is "+nameLength);
+    }
+}
