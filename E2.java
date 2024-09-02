5f257a728f192f51cd6fd02a24c59ed5d3b79493
Edvin-Persson <eddepersson@gmail.com>
2024-09-02 15:14:54 +0200

    Changed E2 to exercise 2.2

--- E2.java
+++ E2.java
@@ -3,11 +3,12 @@ public class E2 {
     public static void main(String[] args) {
    
      
-       for(int i = 1; i<= 19; i++){
-        if (!(i%2==0)) {
-            System.out.print(i +" ");
-        }
-       }
+       int result = 140;
+
+       for(int i = 20; i > 15; i--){
+        result = result - i;
+        System.out.println(result);
+       }System.out.println(result);
 
 
     }
