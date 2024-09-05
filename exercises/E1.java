b6150765f6bc24fc1d158f618194e2de268d2f8d
Edvin-Persson <eddepersson@gmail.com>
2024-09-05 16:54:51 +0200

    Done with 1.14

--- exercises/E1.java
+++ exercises/E1.java
@@ -2,13 +2,17 @@ public class E1 {
 
     public static void main(String[] args) {
         
-      int number = 16;
+      int number = 9;
 
-      if (number % 2 == 0) {
+      if (number % 2 == 0 && number > 10) {
         
-        System.out.println("The number is even");
-      }else{
-        System.out.println("The number is odd");
+        System.out.println("The number is even and greater than 10");
+      }else if (!(number % 2 == 0) && number > 10) {
+        System.out.println("The number is odd and greater than 10");
+      }else if (!(number % 2 == 0) && number < 10){
+        System.out.println("The number is odd and less than 10");
+      } else if ((number % 2 == 0) && number < 10) {
+        System.out.println("The number is even and less than 10");
       }
         
     }
