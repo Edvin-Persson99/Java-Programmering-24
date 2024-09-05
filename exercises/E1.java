c3c33d46bb063107853e2c397d275db73f22b8a5
Edvin-Persson <eddepersson@gmail.com>
2024-09-05 15:20:19 +0200

    Changed to 1.14

--- exercises/E1.java
+++ exercises/E1.java
@@ -2,13 +2,14 @@ public class E1 {
 
     public static void main(String[] args) {
         
-        String firstName = "Edvin";
+      int number = 16;
 
-        String lastName = "Persson";
-
-        String name = firstName + lastName;
-
-        System.out.println("The length of my firstName and lastName is "+name.length());
+      if (number % 2 == 0) {
+        
+        System.out.println("The number is even");
+      }else{
+        System.out.println("The number is odd");
+      }
         
     }
 
