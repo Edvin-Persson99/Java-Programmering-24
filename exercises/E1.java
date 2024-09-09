76fac8c37d3e2bafa9b314e12958d93326adc710
Edvin-Persson <eddepersson@gmail.com>
2024-09-09 11:08:34 +0200

    I am at 1.15

--- exercises/E1.java
+++ exercises/E1.java
@@ -2,18 +2,12 @@ public class E1 {
 
     public static void main(String[] args) {
         
-      int number = 9;
+    
+      String sentence = "I have been in the city";
 
-      if (number % 2 == 0 && number > 10) {
-        
-        System.out.println("The number is even and greater than 10");
-      }else if (!(number % 2 == 0) && number > 10) {
-        System.out.println("The number is odd and greater than 10");
-      }else if (!(number % 2 == 0) && number < 10){
-        System.out.println("The number is odd and less than 10");
-      } else if ((number % 2 == 0) && number < 10) {
-        System.out.println("The number is even and less than 10");
-      }
+      int index = sentence.indexOf("the");
+
+      System.out.println("The word 'the' was found at "+index);
         
     }
 
