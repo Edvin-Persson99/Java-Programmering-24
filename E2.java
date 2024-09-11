eab5a05333a3a3edc323bff3c3931c134b1aedbb
Edvin-Persson <eddepersson@gmail.com>
2024-09-11 10:07:47 +0200

    Did 2.2e

--- E2.java
+++ E2.java
@@ -3,9 +3,12 @@ public class E2 {
     public static void main(String[] args) {
    
      
-        int result = 0;
-        while (result <= 100){
-            result = result + 16;
+        int result = 0, a = 0;
+        while (result <= 100 & a <= 10) {
+            result = result + a;
+            // 0, 3, 9, 18
+            a = a + 3;
+            // a = 3, 6, 9 
             System.out.println(result);
         }
             
