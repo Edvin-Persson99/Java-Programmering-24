0ae689abc527236c37d54015f4fa502fc3c70e1c
Edvin-Persson <eddepersson@gmail.com>
2024-09-12 12:36:27 +0200

    Created Main.java

---
+++ exercises/challenges/Main.java
@@ -0,0 +1,15 @@
+package exercises.challenges;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Write a number here: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 
+
+
+
+    }
+
+}
