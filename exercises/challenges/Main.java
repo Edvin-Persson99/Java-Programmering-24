624389ba4142ab2bf3ae120a08db7a00cef1ed51
Edvin-Persson <eddepersson@gmail.com>
2024-09-12 13:15:25 +0200

    Started challange 1.3

--- exercises/challenges/Main.java
+++ exercises/challenges/Main.java
@@ -6,32 +6,16 @@ public class Main {
         System.out.print("Write a number here: ");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Write the first number here: ");
-        int number1 = scanner.nextInt();
-
-        System.out.print("Write the second number here: ");
-        int number2 = scanner.nextInt();
-
-        System.out.print("Write the third number here: ");
-        int number3 = scanner.nextInt();
-
+        double number = scanner.nextDouble();
         
-
-        if (number1 == number2 && number2 == number3) {
-            System.out.println("All numbers are the same");
+        if (number == 0) {
+            System.out.println("0");
         }
-
-        if (number1 > number2 && number1 > number3) {
-            System.out.println(number1+" is the biggest number");
+        if (number % 2 == 0) {
+            System.out.println("Positive");
         }
-
-
-        if (number2 > number1 && number2 > number3) {
-            System.out.println(number2 + " is the biggest number");
-        }
-
-        if (number3 > number1 && number3 > number2) {
-            System.out.println(number3 + " is the biggest number");
+        if (number % 2 != 0) {
+            System.out.println("Negative");
         }
     }
 
