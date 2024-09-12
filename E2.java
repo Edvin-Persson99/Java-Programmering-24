bc4995dfa7fd1a656d8c1c88061ab228603c8c9f
Edvin-Persson <eddepersson@gmail.com>
2024-09-12 10:02:01 +0200

    Did 2.3b

--- E2.java
+++ E2.java
@@ -3,19 +3,17 @@ public class E2 {
     public static void main(String[] args) {
    
      
-        int result = 0;
-        for (int i = 0; i <= 10; i++) {
-            if (i % 2 != 0)
-                continue;
-            result = result + i;
+        int result = 100;
+        for (int i = 0; i < 100; i++) {
+            if (i == 11)
+                break;
+            result = result - i;
             System.out.println(result);
-            //result = 0, 2, 6, 12, 20, 30
-
-            
-        
         }
             
-         
+         // Result = 100, 99, 97, 94, 90, 85, 79, 72, 64, 55, 45        
+         // i = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+         //Resultatet blir 45 eftersom även om for loopen går till 99 så avbryts den när i är 11 eftersom det finns ett break statement 
         
 
     }
