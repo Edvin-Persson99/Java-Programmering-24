5ea4fb2d2cd83a5feb65cf3c0123dca38163f702
Edvin-Persson <eddepersson@gmail.com>
2024-09-12 11:11:39 +0200

    Done with 2.4

--- E2.java
+++ E2.java
@@ -1,20 +1,29 @@
 public class E2 {
 
     public static void main(String[] args) {
-   
-     
-        int result = 100;
-        for (int i = 0; i < 100; i++) {
-            if (i == 11)
-                break;
-            result = result - i;
-            System.out.println(result);
-        }
-            
-         // Result = 100, 99, 97, 94, 90, 85, 79, 72, 64, 55, 45        
-         // i = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-         //Resultatet blir 45 eftersom även om for loopen går till 99 så avbryts den när i är 11 eftersom det finns ett break statement 
+
+      
+
         
+   
+     for(int i = 1,counter = 0, sum = 0; i < 100; i++){
+
+
+        if (counter == 10) {
+            System.out.println(sum);
+            break;
+            
+        }
+
+        if (i % 2 != 0) {
+            sum+=i;
+            counter++;
+            System.out.println(sum);
+        }
+
+     }
+
+
 
     }
 
