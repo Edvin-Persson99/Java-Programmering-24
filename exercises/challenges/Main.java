2ddab5266ba26265f0c41635c643907646df6e7b
Edvin-Persson <eddepersson@gmail.com>
2024-09-13 10:43:09 +0200

    Did challenge 1.5

--- exercises/challenges/Main.java
+++ exercises/challenges/Main.java
@@ -3,7 +3,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-     
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write a letter here: ");
+        String VOWELS = "aeiouåäöAEIOUÅÄÖ";
+        String Consonants = "bcdfghjklmnpqrstvxzBCDFGHJKLMNPQRSTVXZ";
+
+        String charachter = scanner.nextLine();
+
+        while (charachter.length() != 1) {
+            System.out.println("You can only have one letter");
+            System.out.print("Write a letter here: ");
+            charachter = scanner.nextLine();
+        }
+        
+        if (VOWELS.contains(charachter)) {
+            System.out.println(charachter+" is a vowel");
+        }
+
+        if (Consonants.contains(charachter)) {
+            System.out.println(charachter+ " is a charachter");
+        }
+
+        else if (!Consonants.contains(charachter)&&!VOWELS.contains(charachter)) {
+            System.out.println("You did the wrong thing");
+        }
     }
 
 }
