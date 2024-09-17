f48a381592bcd07ef02d185b8f747130597e87eb
Edvin-Persson <eddepersson@gmail.com>
2024-09-17 09:03:06 +0200

    Created the theropod class again in another folder

---
+++ Classes and Objects/Theropod.java
@@ -0,0 +1,15 @@
+
+
+public class Theropod {
+   
+        String species;
+        int age;
+        double skullLength;
+
+        public static void main(String[] args) {
+
+        
+
+    }
+
+}
