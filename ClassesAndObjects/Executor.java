5018ba2c9b807f5a058e9de9623172fa1d239e49
Edvin-Persson <eddepersson@gmail.com>
2024-09-17 10:41:42 +0200

    Created the executor class with an instance of the container class

--- ClassesAndObjects/Executor.java
+++ ClassesAndObjects/Executor.java
@@ -1,5 +1,16 @@
 package ClassesAndObjects;
 
 public class Executor {
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.albertosaurus.species = "albertosaurus";
+        container.albertosaurus.age = 30;
+        container.albertosaurus.skullLenght = 120.0;
+
+
+        container.trex.species = "Tyrannosaurus rex";
+        container.trex.age = 20;
+        container.trex.skullLenght = 200.4;
+    }
 
 }
