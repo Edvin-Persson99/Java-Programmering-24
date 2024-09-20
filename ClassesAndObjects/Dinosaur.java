230f2e3cb31c1e2028c8d889c9487cb997d504ad
Edvin-Persson <eddepersson@gmail.com>
2024-09-20 11:32:31 +0200

    Added a walk method in the dinosaur class

--- ClassesAndObjects/Dinosaur.java
+++ ClassesAndObjects/Dinosaur.java
@@ -2,4 +2,11 @@ package ClassesAndObjects;
 
 public class Dinosaur {
     boolean walksWithTheirLegsUnderTheirBody = true;
+
+
+    public static void walk(){
+        System.out.println("The dinosaur is walking");
+    }
 }
+
+
