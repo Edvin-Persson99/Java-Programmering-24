1bd837346ae6750c9e33d3238d7f523ae9fc1135
Edvin-Persson <eddepersson@gmail.com>
2024-09-20 14:15:03 +0200

    Done with 1.15

--- exercises/E1.java
+++ exercises/E1.java
@@ -3,12 +3,17 @@ public class E1 {
     public static void main(String[] args) {
         
     
-      String sentence = "I have been in the city";
+      String sentence = "I have been in a city";
 
       int index = sentence.indexOf("the");
 
-      System.out.println("The word 'the' was found at "+index);
-        
+      
+      
+      if (sentence.contains("the")) {
+        System.out.println("The word 'the' was found at index "+index);
+      }else{
+        System.out.println("The word 'the' was not found at all");
+      }
     }
 
 }
