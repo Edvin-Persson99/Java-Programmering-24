509cf0e487cce9de4f94f54800074b168039c70d
Edvin-Persson <eddepersson@gmail.com>
2024-09-23 13:06:33 +0200

    Done with circle class

--- exercises/E4exercsies/E4/Circle.java
+++ exercises/E4exercsies/E4/Circle.java
@@ -1,5 +1,17 @@
 package exercises.E4exercsies.E4;
-
+import java.math.*;
 public class Circle {
+
+    private double radius;
+
+    private double diameter;
+
+    public double calculateCircumference(){
+        return Math.PI *this.diameter;
+    }
+
+    public double calculateArea(){
+        return Math.PI*Math.pow(this.radius, 2.0); 
+    }
 
 }
