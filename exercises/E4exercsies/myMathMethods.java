9d388a79123de6054e6926d87f6ddf4deef4a399
Edvin-Persson <eddepersson@gmail.com>
2024-09-23 15:04:37 +0200

    Did the first two math methods

---
+++ exercises/E4exercsies/myMathMethods.java
@@ -0,0 +1,37 @@
+package exercises.E4exercsies;
+
+public class myMathMethods {
+
+    public static void main(String[] args) {
+        calculateFactorial(5);
+        calculateFibonacci(4);
+    }
+
+    static void calculateFactorial(int number){
+
+        
+
+        int i, fact = 1;
+        for(i = 1; i <= number; i++){
+            fact = fact*i;
+        }
+        System.out.println("Factorial of " + number + " is: " + fact);
+    }
+
+    static void calculateFibonacci(int number){
+
+        int num1 = 0, num2 = 1;
+
+        for (int i = 0; i < number; i++) {
+            // Print the number
+            System.out.print(num1 + " ");
+
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+        }
+        
+    }
+
+}
