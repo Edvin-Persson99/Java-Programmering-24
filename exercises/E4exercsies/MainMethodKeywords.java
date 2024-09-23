7aec208bf5c16b89595dce8a0a1bbbeb867ea3e4
Edvin-Persson <eddepersson@gmail.com>
2024-09-23 12:45:12 +0200

    Done with first exercise

--- exercises/E4exercsies/MainMethodKeywords.java
+++ exercises/E4exercsies/MainMethodKeywords.java
@@ -2,7 +2,13 @@ package exercises.E4exercsies;
 
 public class MainMethodKeywords {
 
-    public static void main(String[] args) {
+    public static void main(int argument) {
         System.out.print("main is working fine!");
     }
+
+    
 }
+
+ /*The main method must return void beacuse the java program 
+ terminates when the main method terminates
+*/
